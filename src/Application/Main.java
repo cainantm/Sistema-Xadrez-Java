008f5b91f09b48eba26e862be2228f637552b79b
Cainan Machado <cainantm@hotmail.com>
2023-07-18 15:53:52 -0300

    Camada Chess e Print Board

--- src/Application/Main.java
+++ src/Application/Main.java
@@ -2,11 +2,13 @@ package Application;
 
 import Boardgame.Board;
 import Boardgame.Position;
+import Chess.ChessMatch;
 
 public class Main {
     public static void main(String[] args) {
 
-        Board board = new Board(8,8);
+        ChessMatch chessMatch = new ChessMatch();
+        UI.printBoard(chessMatch.getPieces());
 
 
     }
