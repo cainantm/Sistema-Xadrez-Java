861f146e642cfd7e31d8c9f66581fb8cd50e839e
Cainan Machado <cainantm@hotmail.com>
2023-07-24 16:50:18 -0300

    Criando classe Bispo

--- src/Chess/ChessMatch.java
+++ src/Chess/ChessMatch.java
@@ -3,6 +3,7 @@ package Chess;
 import Boardgame.Board;
 import Boardgame.Piece;
 import Boardgame.Position;
+import Chess.pieces.Bishop;
 import Chess.pieces.King;
 import Chess.pieces.Pawn;
 import Chess.pieces.Rook;
@@ -131,6 +132,8 @@ public class ChessMatch {
     }
 
     private void initialSetup(){
+        placeNewPiece('c', 1, new Bishop(board, Color.WHITE));
+        placeNewPiece('f', 1, new Bishop(board, Color.WHITE));
         placeNewPiece('a', 1, new Rook(board, Color.WHITE));
         placeNewPiece('e', 1, new King(board, Color.WHITE));
         placeNewPiece('h', 1, new Rook(board, Color.WHITE));
@@ -144,6 +147,8 @@ public class ChessMatch {
         placeNewPiece('h', 2, new Pawn(board, Color.WHITE));
 
         placeNewPiece('a', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('f', 8, new Bishop(board, Color.BLACK));
+        placeNewPiece('c', 8, new Bishop(board, Color.BLACK));
         placeNewPiece('e', 8, new King(board, Color.BLACK));
         placeNewPiece('h', 8, new Rook(board, Color.BLACK));
         placeNewPiece('a', 7, new Pawn(board, Color.BLACK));
