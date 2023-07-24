55ccb344702edc2e632c3f38be26ff3919d2f574
Cainan Machado <cainantm@hotmail.com>
2023-07-24 17:17:34 -0300

    Adicionando movimentos da Rainha

--- src/Chess/ChessMatch.java
+++ src/Chess/ChessMatch.java
@@ -129,6 +129,7 @@ public class ChessMatch {
     }
 
     private void initialSetup(){
+        placeNewPiece('d', 1, new Queen(board, Color.WHITE));
         placeNewPiece('b', 1, new Knight(board, Color.WHITE));
         placeNewPiece('g', 1, new Knight(board, Color.WHITE));
         placeNewPiece('c', 1, new Bishop(board, Color.WHITE));
@@ -145,6 +146,7 @@ public class ChessMatch {
         placeNewPiece('g', 2, new Pawn(board, Color.WHITE));
         placeNewPiece('h', 2, new Pawn(board, Color.WHITE));
 
+        placeNewPiece('d', 8, new Queen(board, Color.BLACK));
         placeNewPiece('a', 8, new Rook(board, Color.BLACK));
         placeNewPiece('f', 8, new Bishop(board, Color.BLACK));
         placeNewPiece('c', 8, new Bishop(board, Color.BLACK));
